106b95c0b41cc5e8ef0df81e3a6c46f05347257e
longpiseth2002 <stevkhat121212@gmail.com>
2024-02-07 15:21:30 +0700

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println(" Hello world ");
-=======
         System.out.println("Group 1 is here");
->>>>>>> b6fd866d5a6175b89df715b715d79c02b22708a6
     }
 }
